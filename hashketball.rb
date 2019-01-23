e42ac06f26ab501bb2faec82d2913c58534c792c
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-23 15:48:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,7 @@ def player_by_number(num)
   game_hash.each do |location, team_data|
     binding.pry
      team_data[:number].each do |name,data|
-       
+    end   
 end
 
 player_by_number(33)
